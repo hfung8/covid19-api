a662e6fa9e32db89d37d39ced6a3d0601c192198
hfung8 <harrison.hok.fung@gmail.com>
2025-02-24 11:21:05 -0500

    updated file

--- api/raw/cases/province-state.ts
+++ api/raw/cases/province-state.ts
@@ -16,8 +16,7 @@ export default async (req: NowRequest, res: NowResponse) => {
     typeof req.query.resultOffset === "string"
       ? parseInt(req.query.resultOffset, 10)
       : 0;
-  const field =
-    req.query.field in sortBy ? (req.query.field as string) : "confirmed";
+  const field = req.query.field in sortBy ? (req.query.field as string) : "confirmed";
   const query = createSortQuery(createCountySortGroup(field), { resultOffset });
   const response = await fetcher(`${endpoint}?${qs.stringify(query)}`);
   const data = await response.json();
